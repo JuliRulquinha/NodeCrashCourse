d94df9996749288f5bcb20978d518881a0a2bd43
JuliRulquinha <juli-felix@outlook.com>
2025-05-14 17:20:56 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+import getPosts, {getPostsLength} from './postController.js';
+console.log(getPosts());
+console.log(`Total posts: ${getPostsLength()}`);
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(2)}`);
+
